dac95abdf7658b065b11ef3d0edeadc8afc117c3
Fredrik <fredrik.christenson@gmail.com>
2018-03-24 06:42:23 +0100

    comment

--- src/public/js/components/subheading/index.js
+++ src/public/js/components/subheading/index.js
@@ -1,5 +1,7 @@
 let Component = () => null;
 
+// it is important to use if statements since webpack
+// will add all required modules in a switch in to the bundle
 if (__LANGUAGE__ === "en") Component = require(`./en`);
 
 // this should only be in the english version
